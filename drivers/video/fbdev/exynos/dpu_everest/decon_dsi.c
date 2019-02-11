0cad6ff65ae7546b3cade3318085870b7fcdd85c
Diep Quynh <remilia.1505@gmail.com>
2019-02-11 13:12:46 +0700

    fbdev: decon: Set kthread priorities
    This is to match with mali's worker thread
    All of them must have the same priority, as they're critical threads
    responsible for display rendering
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/video/fbdev/exynos/dpu_everest/decon_dsi.c
+++ drivers/video/fbdev/exynos/dpu_everest/decon_dsi.c
@@ -474,6 +474,7 @@ static int decon_vsync_thread(void *data)
 
 int decon_create_vsync_thread(struct decon_device *decon)
 {
+	struct sched_param param = { .sched_priority = 20 };
 	int ret = 0;
 	char name[16];
 
@@ -496,6 +497,8 @@ int decon_create_vsync_thread(struct decon_device *decon)
 		ret = PTR_ERR(decon->vsync.thread);
 		goto err;
 	}
+
+	sched_setscheduler_nocheck(decon->vsync.thread, SCHED_FIFO, &param);
 
 	return 0;
 
@@ -543,6 +546,7 @@ static int decon_fsync_thread(void *data)
 
 int decon_create_fsync_thread(struct decon_device *decon)
 {
+	struct sched_param param = { .sched_priority = 20 };
 	char name[16];
 
 	if (decon->dt.out_type != DECON_OUT_DSI) {
@@ -557,6 +561,8 @@ int decon_create_fsync_thread(struct decon_device *decon)
 		decon->fsync.thread = NULL;
 		return PTR_ERR(decon->fsync.thread);
 	}
+
+	sched_setscheduler_nocheck(decon->fsync.thread, SCHED_FIFO, &param);
 
 	return 0;
 }
