78de8047d2c515e246efb9e326c3599f7be7ec34
Dylan Neve <dylanneve1@gmail.com>
2023-05-07 22:51:46 +0100

    Revert "fbdev: dpu20: mem/unmap ref cnt debug feature enable"
    This reverts commit 9c0c90eee291119d0d3e73e38c67f625a4bd2aa9.

--- drivers/video/fbdev/exynos/dpu_everest/decon_core.c
+++ drivers/video/fbdev/exynos/dpu_everest/decon_core.c
@@ -401,10 +401,8 @@ static void decon_free_unused_buf(struct decon_device *decon,
 
 	decon_info("%s, win[%d]plane[%d]\n", __func__, win, plane);
 
-	if (dma->attachment && dma->dma_addr) {
+	if (dma->attachment && dma->dma_addr)
 		ion_iovmm_unmap(dma->attachment, dma->dma_addr);
-		dpu_memmap_dec(decon, dma->dma_addr);
-	}
 	if (dma->attachment && dma->sg_table)
 		dma_buf_unmap_attachment(dma->attachment,
 				dma->sg_table, DMA_TO_DEVICE);
@@ -428,7 +426,6 @@ static void decon_free_dma_buf(struct decon_device *decon,
 	if (dma->fence)
 		fput(dma->fence->file);
 	ion_iovmm_unmap(dma->attachment, dma->dma_addr);
-	dpu_memmap_dec(decon, dma->dma_addr);
 
 	dma_buf_unmap_attachment(dma->attachment, dma->sg_table,
 			DMA_TO_DEVICE);
@@ -1385,7 +1382,6 @@ static unsigned int decon_map_ion_handle(struct decon_device *decon,
 		decon_err("iovmm_map() failed: %pa\n", &dma->dma_addr);
 		goto err_iovmm_map;
 	}
-	dpu_memmap_inc(decon, dma->dma_addr);
 
 	dma->ion_handle = ion_handle;
 
